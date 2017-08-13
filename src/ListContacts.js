a19123277b28ffa1bcd87705afafd11d2368e77c
ordasf <fordas88@gmail.com>
2017-08-13 22:10:49 +0200

    Add message informing the number of result found for a contact search

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -18,14 +18,21 @@ class ListContacts extends Component {
         this.setState({query: query.trim()})
     }
 
+    clearQuery = () => {
+        this.setState({ query: '' })
+    }
+
     render() {
+        const { contacts, onDeleteContact } = this.props
+        const { query } = this.state
+
         let showingContacts
 
-        if (this.state.query) {
-            const regExp = new RegExp(escapeStringRegexp(this.state.query), 'i')
-            showingContacts = this.props.contacts.filter((contact) => regExp.test(contact.name))
+        if (query) {
+            const regExp = new RegExp(escapeStringRegexp(query), 'i')
+            showingContacts = contacts.filter((contact) => regExp.test(contact.name))
         } else {
-            showingContacts = this.props.contacts
+            showingContacts = contacts
         }
 
         showingContacts.sort(sortBy('name'))
@@ -37,9 +44,17 @@ class ListContacts extends Component {
                     className="search-contacts"
                     type="text"
                     placeholder="Search contact"
-                    value={this.state.query}
+                    value={query}
                     onChange={(event) => this.updateQueryMethod(event.target.value)}/>
             </div>
+
+            {showingContacts.length !== contacts.length && (
+                <div className="showing-contacts">
+                    <span>Showing {showingContacts.length} of {contacts.length} contacts</span>
+                    <button onClick={() => this.clearQuery()}>Show all</button>
+                </div>
+            )}
+
             <ol className="contact-list">
               {showingContacts.map( (contact) =>
                 <li key={contact.id} className='contact-list-item'>
@@ -50,7 +65,7 @@ class ListContacts extends Component {
                     <p>{contact.name}</p>
                     <p>{contact.email}</p>
                   </div>
-                  <button className='contact-remove' onClick={() => {this.props.onDeleteContact(contact)}}>
+                  <button className='contact-remove' onClick={() => {onDeleteContact(contact)}}>
                     Remove
                   </button>
                 </li>)}
